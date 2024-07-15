d0215793ef2d5f86f4988ef0ef6a6bbd222f5d1f
juan.paz.h <juan.paz.h@gmail.com>
2024-07-15 22:00:26 +0000

    embeddings import

--- urlai_app/app.py
+++ urlai_app/app.py
@@ -4,7 +4,8 @@ import os
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 #from langchain.embeddings import HuggingFaceEmbeddings 
-from langchain_community.embeddings import HuggingFaceEmbeddings
+#from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 #from langchain.vectorstores import FAISS
 from langchain_community.vectorstores import FAISS
 #from langchain.chat_models import ChatOpenAI
