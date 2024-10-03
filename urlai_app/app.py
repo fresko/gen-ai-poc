4a2766d8981098f91fbd8cc0f115ab1ef7bf23ce
juan.paz.h <juan.paz.h@gmail.com>
2024-10-03 06:51:37 +0000

    cambio de llm a gemini

--- urlai_app/app.py
+++ urlai_app/app.py
@@ -11,10 +11,13 @@ from langchain_community.vectorstores import FAISS
 #from langchain.chat_models import ChatOpenAI
 from langchain_community.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 
 st.set_page_config('preguntaDOC')
 st.header("Pregunta a tu PDF")
-OPENAI_API_KEY = st.text_input('OpenAI API Key', type='password')
+#OPENAI_API_KEY = st.text_input('OpenAI API Key', type='password')
+GOOGLE_API_KEY = st.text_input('GOOGLE_API_KEY', type='password')
 pdf_obj = st.file_uploader("Carga tu documento", type="pdf", on_change=st.cache_resource.clear)
 
 @st.cache_resource 
@@ -34,16 +37,18 @@ def create_embeddings(pdf):
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
     knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-    return knowledge_base
+return knowledge_base
 
 if pdf_obj:
     knowledge_base = create_embeddings(pdf_obj)
     user_question = st.text_input("Haz una pregunta sobre tu PDF:")
 
     if user_question:
-        os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+        #os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+        os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
         docs = knowledge_base.similarity_search(user_question, 3)
-        llm = ChatOpenAI(model_name='gpt-3.5-turbo')
+        #llm = ChatOpenAI(model_name='gpt-4o') #gpt-4o
+        llm = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.3)
         chain = load_qa_chain(llm, chain_type="stuff")
         respuesta = chain.run(input_documents=docs, question=user_question)
 
