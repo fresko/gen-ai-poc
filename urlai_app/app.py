c9b90fd6088eb444b1096bfb81f6258ac28a663f
juan.paz.h <juan.paz.h@gmail.com>
2024-10-04 19:47:16 +0000

    embeddings chage

--- urlai_app/app.py
+++ urlai_app/app.py
@@ -5,7 +5,7 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 #from langchain.embeddings import HuggingFaceEmbeddings 
 #from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_huggingface import HuggingFaceEmbeddings
+#from langchain_huggingface import HuggingFaceEmbeddings
 #from langchain.vectorstores import FAISS
 from langchain_community.vectorstores import FAISS
 #from langchain.chat_models import ChatOpenAI
