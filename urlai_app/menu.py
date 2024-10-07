d9678c1e36c155a9e77f176299c631a052f7d412
juan.paz.h <juan.paz.h@gmail.com>
2024-10-07 01:50:01 +0000

    stl

--- urlai_app/menu.py
+++ urlai_app/menu.py
@@ -11,7 +11,7 @@ def streamlit_menu(example=1):
         with st.sidebar:
             selected = option_menu(
                 menu_title="Main Menu",  # required
-                options=["Start", "AI Agent", "Contact"],  # required
+                options=["Start", "AI-Agent", "Contact"],  # required
                 icons=["rocket", "robot", "envelope"],  # optional
                 menu_icon="cast",  # optional
                 default_index=0,  # optional
@@ -22,7 +22,7 @@ def streamlit_menu(example=1):
         # 2. horizontal menu w/o custom style
         selected = option_menu(
             menu_title=None,  # required
-            options=["Start", "AI Agent", "Contact"],  # required
+            options=["Start", "AI-Agent", "Contact"],  # required
             icons=["rocket", "robot", "envelope"],  # optional
             menu_icon="cast",  # optional
             default_index=0,  # optional
@@ -34,7 +34,7 @@ def streamlit_menu(example=1):
         # 2. horizontal menu with custom style
         selected = option_menu(
             menu_title=None,  # required
-            options=["Start", "AI Agent", "Contact"],  # required
+            options=["Start", "AI-Agent", "Contact"],  # required
             icons=["rocket", "robot", "envelope"],  # optional
             menu_icon="cast",  # optional
             default_index=0,  # optional
@@ -56,11 +56,20 @@ def streamlit_menu(example=1):
 
 selected = streamlit_menu(example=EXAMPLE_NO)
 
-#tab1, tab2, tab3 = st.tabs(["Google - Gemini", "OpenAI - ChatGPT", "Conversacional Data Business "])
 
-if selected == "Home":
-    st.title(f"You have selected {selected}")
-if selected == "Projects":
-    st.title(f"You have selected {selected}")
+if selected == "Start":
+    st.title(f"Hello Space People ! {selected}")
+if selected == "AI-Agent":
+    st.title(f"{selected} - Proof of Concept  ")
+    tab1, tab2, tab3 = st.tabs(["Google - Gemini", "OpenAI - ChatGPT", "Conversational Data Business "])
+    with tab1:
+        st.header("Google - Gemini")
+        st.image("https://1000marcas.net/wp-content/uploads/2024/02/Gemini-Logo.jpg", width=100)
+    with tab2:
+        st.header("OpenAI - ChatGPT")
+        st.image("https://www.androidheadlines.com/wp-content/uploads/2023/03/GPT-4-logo-1420x799.webp",  width=100)
+    with tab3:
+        st.header("Conversational Data Business ")
+        st.image("https://www.researchgate.net/profile/Marianna-Charitonidou/publication/360719662/figure/fig1/AS:1157512099299329@1652983798438/DATA-TUNNEL-2020-21-Custom-software-site-specific-installation-Duration-9-minutes.jpg", width=100)
 if selected == "Contact":
     st.title(f"You have selected {selected}")
